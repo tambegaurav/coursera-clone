21945c869b4d77db4299c3e6b6df97e161d82314
Gaurav <gauravkishortambe@gmail.com>
2021-05-27 13:05:06 +0530

    correct eslint setup

--- client/.eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: ['airbnb'],
-  rules: {
-    'react/jsx-filename-extension': 'off',
-    //You can override any rules you want
-  },
-};
--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Routes from './Routes/Routes';
 
 function App() {
   return (
     <div className="App">
-      <h1>Coursera Clone</h1>
+      <Routes />
     </div>
   );
 }
