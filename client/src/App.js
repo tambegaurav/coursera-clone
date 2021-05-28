e534ac7464ccf91ffb1db5cce889c94075d77a7c
Vedansh Wani <me.vedanshwani@gmail.com>
2021-05-28 14:39:28 +0530

    home hero section

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+
 import Routes from './Routes/Routes';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Routes />
     </div>
   );
--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Footer from '../../Shared-Components/Footer';
 import Navbar from '../../Shared-Components/Navbar';
+import { HomeBanner } from '../../Shared-Components/Home/HomeBanner/HomeBanner';
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <h1>Home</h1>
+      <HomeBanner />
       <Footer />
     </div>
   );
