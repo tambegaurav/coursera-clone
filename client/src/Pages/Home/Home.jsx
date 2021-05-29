36de3c31aa3685dbe273a9b9f1780aaf454b1215
Gaurav <gauravkishortambe@gmail.com>
2021-05-29 19:44:22 +0530

    add reviews on homepage

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Footer from '../../Shared-Components/Footer';
 import Navbar from '../../Shared-Components/Navbar';
 import { HomeBanner } from '../../Shared-Components/Home/HomeBanner/HomeBanner';
+// eslint-disable-next-line max-len
+import CommunityReviewSection from '../../Shared-Components/Home/CommunityReviewSection/CommunityReviewSection';
 
 const Home = () => {
   return (
@@ -9,6 +11,7 @@ const Home = () => {
       <Navbar />
       <h1>Home</h1>
       <HomeBanner />
+      <CommunityReviewSection />
       <Footer />
     </div>
   );
