4c6878ad1403b8dcb226b699c16e0a599a88bb1a
Gaurav <gauravkishortambe@gmail.com>
2021-06-10 00:10:01 +0530

    fetching all the videos of particular course on admin side

--- client/src/Pages/AdminCoursePage/AdminCoursePage.jsx
+++ client/src/Pages/AdminCoursePage/AdminCoursePage.jsx
@@ -1,6 +1,24 @@
-import React from 'react';
+/* eslint-disable spaced-comment */
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { fetchAllVideosParticularCourse } from '../../Redux/Admin/Course/actions';
 
 const AdminCoursePage = () => {
+  const params = useParams();
+  const [courseId, setCourseId] = useState('');
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    setCourseId(params.id);
+  }, [params]);
+
+  useEffect(() => {
+    if (courseId !== '') {
+      dispatch(fetchAllVideosParticularCourse(courseId));
+    }
+  }, [courseId]);
+
   return (
     <div>
       <h1>AdminCoursePage</h1>
