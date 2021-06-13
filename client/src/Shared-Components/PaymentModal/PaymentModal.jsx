35e314b1bcc4706cd84cb20b5d9ffaaf44c7d67f
Gaurav <gauravkishortambe@gmail.com>
2021-06-14 01:51:59 +0530

    add payment modal with razorpay

---
+++ client/src/Shared-Components/PaymentModal/PaymentModal.jsx
@@ -0,0 +1,46 @@
+/* eslint-disable react/prop-types */
+import { Button } from '@material-ui/core';
+import React from 'react';
+import styled from 'styled-components';
+import PayByRazorPay from './RazorpayBtn';
+
+const Box = styled.div`
+  background-color: #0156d1;
+  width: 700px;
+  border-radius: 7px;
+  margin-top: 200px;
+  padding: 10px 20px;
+  color: white;
+
+  & > h1 {
+    margin-top: 50px;
+    text-align: center;
+  }
+`;
+
+const Buttons = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-around;
+  margin-top: 30px;
+  margin-bottom: 20px;
+
+  > .MuiButtonBase-root {
+    color: white;
+    font-size: 18px;
+  }
+`;
+
+const PaymentModal = ({ handleClose }) => {
+  return (
+    <Box>
+      <h1>Are you sure, you want to purchase this Course?</h1>
+      <Buttons>
+        <PayByRazorPay amount={500} handleClose={handleClose} />
+        <Button onClick={handleClose}>Cancel</Button>
+      </Buttons>
+    </Box>
+  );
+};
+
+export default PaymentModal;
