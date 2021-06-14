a990949ad4dacc65db97b497289ca8af11e6d836
Gaurav <gauravkishortambe@gmail.com>
2021-06-15 02:44:57 +0530

    add upload img feature while adding new courses

--- client/src/Pages/AdminNewCourseForm/NewCourseForm.jsx
+++ client/src/Pages/AdminNewCourseForm/NewCourseForm.jsx
@@ -10,11 +10,13 @@ import {
   MenuItem,
   Button,
 } from '@material-ui/core';
+import { v4 as uuid } from 'uuid';
 import { useDispatch } from 'react-redux';
 import useStyles from './styles';
 import { categories, levels, obj } from './SelectTagValues';
 import { addCourse } from '../../Redux/Admin/Course/actions';
 import fileUpload from '../../CustomHooks/fileUpload';
+import { storage } from '../../utils/firebase';
 
 export const NewCourseForm = () => {
   const classes = useStyles();
@@ -29,9 +31,36 @@ export const NewCourseForm = () => {
   };
 
   const handleSubmit = async () => {
-    const url = await fileUpload(file);
-    console.log(url);
-    dispatch(addCourse(formdata)).then(() => alert('Course Added'));
+    // Create a storage reference from our storage service
+    const storageRef = storage.ref();
+    // Create a child reference
+    const id = uuid();
+    const imagesRef = storageRef.child(`images/${id}`);
+    // imagesRef now points to 'images'
+    const uploadTask = imagesRef.put(file);
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        // eslint-disable-next-line prefer-template
+        console.log('Upload is ' + progress + '% done');
+      },
+      (error) => {
+        // Handle unsuccessful uploads
+        console.log(error);
+      },
+      () => {
+        // Handle successful uploads on complete
+        uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
+          console.log('File available at', downloadURL);
+
+          dispatch(addCourse({ ...formdata, course_img: downloadURL })).then(
+            () => alert('Course Added'),
+          );
+        });
+      },
+    );
   };
 
   const handleFileSelect = (e) => {
